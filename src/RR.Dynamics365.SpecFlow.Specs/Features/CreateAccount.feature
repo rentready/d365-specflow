f8fae51aba6b7a4263ee031ab8e80a3db5b9a48b
Leonid Netrebskii <leonid.netrebskii@rentready.com>
2022-11-23 10:49:39 +0400

    Add specs testing using FakeXrmEasy

---
+++ src/RR.Dynamics365.SpecFlow.Specs/Features/CreateAccount.feature
@@ -0,0 +1,9 @@
+﻿Feature: CreateAccount
+
+Scenario: Should Create an Account
+	Given an account named MainAccount created with the following values
+		| Property | Value            |
+		| name     | Awersome Account |
+	Then I expect an account exists with the following values
+		| Property | Value            |
+		| name     | Awersome Account |
---
+++ src/RR.Dynamics365.SpecFlow.Specs/Hooks/TestFixture.cs
@@ -0,0 +1,34 @@
+﻿using BoDi;
+using FakeItEasy;
+using FakeXrmEasy;
+using RR.Dynamics365.Model;
+using RR.Dynamics365.SpecFlow.Specs.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Vermaat.Crm.Specflow;
+
+namespace RR.Dynamics365.SpecFlow.Specs.Hooks
+{
+    [Binding]
+    internal class TestFixture
+    {
+        private readonly XrmFakedContext _context = new XrmFakedContext();
+
+        [BeforeScenario(Order = 0)]
+        public void BeforeScenarioRun(IObjectContainer objectContainer)
+        {
+            var conn = new FakeCrmConnection("Fake", _context);
+            GlobalTestingContext.ConnectionManager.SetAdminConnection(conn);
+            GlobalTestingContext.ConnectionManager.SetCurrentConnection(conn);
+            objectContainer.RegisterTypeAs<CrmTestingContext, ICrmTestingContext>();
+            objectContainer.RegisterInstanceAs<ISeleniumTestingContext>(A.Fake<ISeleniumTestingContext>());
+
+            var assembly = Assembly.GetAssembly(typeof(Contact));
+            _context.InitializeMetadata(assembly);
+        }
+    }
+}
